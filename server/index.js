3defa46bc97c678135c8dbb931fdb556df737800
Lucas Ferreira <lucas.ferreira@messagebird.com>
2020-12-15 13:39:48 +0100

    add server

---
+++ server/index.js
@@ -0,0 +1,16 @@
+const path = require("path");
+const express = require("express");
+
+const PORT = process.env.PORT || 3000;
+const publicPath = path.join(__dirname, "..", "public");
+const app = express();
+
+app.use(express.static(publicPath));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(publicPath, "index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is up and running on port :${PORT}`);
+});
